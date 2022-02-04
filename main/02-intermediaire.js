fecfa51d3e4f9845ea300ab0a5498e3a04dc10eb
luludegex06 <95019526+Luludegex06@users.noreply.github.com>
2022-02-04 14:11:28 +0100

    02- combine et rotate ok

--- main/02-intermediaire.js
+++ main/02-intermediaire.js
@@ -3,9 +3,16 @@
  * 
  * Exemple : [1, 2, 3] et ["apple", "orange", "banana"] renverra : [1, "apple", 2, "orange", 3, "banana"]
  */
-function combine(tableau1, tableau2) {
-
-}
+function combine(tableau1, tableau2){
+    let combinedTableau = [];
+    for(let i=0; i<tableau1.length; i++){
+        combinedTableau.push(tableau1[i], tableau2[i]);
+    }
+    return combinedTableau;
+};
+let tableau1 = [1, 2, 3];
+let tableau2 = ["apple", "orange", "banana"];
+console.log(combine(tableau1, tableau2));
 
 /**
  * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
@@ -14,8 +21,14 @@ function combine(tableau1, tableau2) {
  * Par exemple : rotate([1, 2, 3, 4, 5], 2) renverra [4, 5, 1, 2, 3] 
  */
 function rotate(tableau, offset) {
-
-}
+    while( offset-- ){
+        let temp = tableau.pop();
+        tableau.unshift(temp);
+    };
+    return tableau;
+};
+let tableau = [1, 2, 3, 4, 5];
+console.log(rotate(tableau, 2));
 
 /**
  * Suite de Syracuse
